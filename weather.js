6698c0e72e11241a1b295bdec81fb46ac6ad7a7d
YanaKorniichuk <114025268+YanaKorniichuk@users.noreply.github.com>
2023-04-28 22:43:07 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -36,7 +36,11 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemp);
 }
-
+function getForecast(coordinates) {
+  let apiKey = "d1a86552de255334f6117b348c4519bd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function showTemp(response) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = response.data.name;
